88005adc77f76053f497d86998cfaea76e2e533a
Uhucream <aborog1324@yahoo.co.jp>
2023-01-06 19:07:17 +0900

    style: ContentView 内において、.environment モディファイアの書かれている位置が見づらいので改行を挿入

--- iOS/ContentView.swift
+++ iOS/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
+            .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
     }
 }
