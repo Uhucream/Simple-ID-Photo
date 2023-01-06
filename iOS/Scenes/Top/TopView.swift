442d4e0af205be9be6a073916db4ade10fdd362f
Uhucream <aborog1324@yahoo.co.jp>
2023-01-06 19:42:24 +0900

    feat: iOS/Scenes/Top に、初期表示画面となる TopView.swift を作成

---
+++ iOS/Scenes/Top/TopView.swift
@@ -0,0 +1,21 @@
+//
+//  TopView.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/01/06
+//  
+//
+
+import SwiftUI
+
+struct TopView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopView()
+    }
+}
