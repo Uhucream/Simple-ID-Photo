7a56ce5b2d2044bfdcf32bb0d437243311dd77b1
Uhucream <aborog1324@yahoo.co.jp>
2023-01-06 19:40:27 +0900

    feat: Scenes/Root/ に、RootView.swift 作成

---
+++ iOS/Scenes/Root/RootView.swift
@@ -0,0 +1,21 @@
+//
+//  RootView.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/01/06
+//  
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RootView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootView()
+    }
+}
