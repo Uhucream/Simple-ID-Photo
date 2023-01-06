9adb38529cde7fc62115a32d2d327a5996f9af2d
Uhucream <aborog1324@yahoo.co.jp>
2023-01-06 20:11:36 +0900

    feat: TopView の内容を作成

--- iOS/Scenes/Top/TopView.swift
+++ iOS/Scenes/Top/TopView.swift
@@ -10,12 +10,43 @@ import SwiftUI
 
 struct TopView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            VStack {
+                Spacer()
+                
+                Text("作成された証明写真がありません")
+                    .foregroundColor(.secondary)
+                
+                Spacer()
+            }
+            .frame(maxHeight: .infinity)
+        }
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Menu {
+                    Button(action: {
+                        
+                    }) {
+                        Label("カメラで撮影", systemImage: "camera")
+                    }
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Label("写真ライブラリから選択", systemImage: "photo.on.rectangle")
+                    }
+                } label: {
+                    Label("create", systemImage: "plus")
+                }
+            }
+        }
     }
 }
 
 struct TopView_Previews: PreviewProvider {
     static var previews: some View {
-        TopView()
+        NavigationView {
+            TopView()
+        }
     }
 }
