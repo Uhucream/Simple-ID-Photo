954b7650ff91ad9baf4be069871740c0adbfff30
Uhucream <aborog1324@yahoo.co.jp>
2023-01-07 14:02:04 +0900

    feat: iOS/Extensions/ に、UIColor の色を定義した Color+ 作成

---
+++ iOS/Extensions/Color+.swift
@@ -0,0 +1,62 @@
+//
+//  Color+.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/01/07
+//  
+//
+
+import SwiftUI
+
+// MARK: Standard content background colors
+extension Color {
+    static let systemBackground = Color(uiColor: .systemBackground)
+    static let secondarySystemBackground = Color(uiColor: .secondarySystemBackground)
+    static let tertiarySystemBackground = Color(uiColor: .tertiarySystemBackground)
+}
+
+// MARK: Grouped content background colors
+extension Color {
+    static let systemGroupedBackground = Color(uiColor: .systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(uiColor: .secondarySystemGroupedBackground)
+    static let tertiarySystemGroupedBackground = Color(uiColor: .tertiarySystemGroupedBackground)
+    
+}
+
+// MARK: Separator colors
+extension Color {
+    static let separator = Color(uiColor: .separator)
+    static let opaqueSeparator = Color(uiColor: .opaqueSeparator)
+}
+
+// MARK: Tint color
+extension Color {
+    static let tintColor = Color(uiColor: .tintColor)
+}
+
+// MARK: Text colors
+extension Color {
+    static let placeholderText = Color(uiColor: .placeholderText)
+}
+
+// MARK: Label colors
+extension Color {
+    static let label = Color(uiColor: .label)
+    static let secondaryLabel = Color(uiColor: .secondaryLabel)
+    static let tertiaryLabel = Color(uiColor: .tertiaryLabel)
+}
+
+// MARK: Link color
+extension Color {
+    static let link = Color(uiColor: .link)
+}
+
+// MARK: Adaptable gray colors
+extension Color {
+    static let systemGray = Color(uiColor: .systemGray)
+    static let systemGray2 = Color(uiColor: .systemGray2)
+    static let systemGray3 = Color(uiColor: .systemGray3)
+    static let systemGray4 = Color(uiColor: .systemGray4)
+    static let systemGray5 = Color(uiColor: .systemGray5)
+    static let systemGray6 = Color(uiColor: .systemGray6)
+}
