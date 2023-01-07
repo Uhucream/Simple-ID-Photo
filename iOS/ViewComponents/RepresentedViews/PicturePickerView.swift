19b6a227d2a7d1d8b573f5c0cf109ab07657f749
Uhucream <aborog1324@yahoo.co.jp>
2023-01-07 19:20:02 +0900

    feat: フォトライブラリから写真を選択できるようにするため、iOS/ViewComponents/RepresentedViews/ に PicturePickerView を作成

---
+++ iOS/ViewComponents/RepresentedViews/PicturePickerView.swift
@@ -0,0 +1,78 @@
+//
+//  PicturePickerView.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/01/07
+//  
+//
+
+import SwiftUI
+import PhotosUI
+
+struct PicturePickerView: UIViewControllerRepresentable {
+    @Environment(\.dismiss) private var dismiss
+
+    @Binding var pictureURL: URL?
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var configuration = PHPickerConfiguration()
+
+        configuration.filter = .images
+        configuration.preferredAssetRepresentationMode = .current
+
+        let picker = PHPickerViewController(configuration: configuration)
+
+        picker.delegate = context.coordinator
+
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parentView: PicturePickerView
+
+        init(_ parent: PicturePickerView) {
+            self.parentView = parent
+        }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+
+            parentView.dismiss()
+
+            guard let provider = results.first?.itemProvider else {
+                return
+            }
+
+            let typeIdentifier = UTType.image.identifier
+            
+            guard provider.hasItemConformingToTypeIdentifier(typeIdentifier) else {
+                return
+            }
+
+            provider.loadFileRepresentation(forTypeIdentifier: typeIdentifier) { url, error in
+                if let error = error {
+                    print("error: \(error)")
+
+                    return
+                }
+                
+                guard let url = url else {
+                    return
+                }
+
+                let fileName = "\(Int(Date().timeIntervalSince1970)).\(url.pathExtension)"
+
+                let newFileURL: URL = .init(fileURLWithPath: NSTemporaryDirectory() + fileName)
+
+                try? FileManager.default.copyItem(at: url, to: newFileURL)
+
+                self.parentView.pictureURL = newFileURL
+            }
+        }
+    }
+}
