29b52b1c3fd5524643a6bfbef77e63fbda328c6f
Uhucream <aborog1324@yahoo.co.jp>
2023-01-12 14:35:43 +0900

    feat: HEX 値で Color を生成できるようにするため、Color+ にイニシャライザ追加

--- iOS/Extensions/Color+.swift
+++ iOS/Extensions/Color+.swift
@@ -8,6 +8,18 @@
 
 import SwiftUI
 
+extension Color {
+    init(_ hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xFF) / 255,
+            green: Double((hex >> 8) & 0xFF) / 255,
+            blue: Double(hex & 0xFF) / 255,
+            opacity: alpha
+        )
+    }
+}
+
 // MARK: Standard content background colors
 extension Color {
     static let systemBackground = Color(uiColor: .systemBackground)
