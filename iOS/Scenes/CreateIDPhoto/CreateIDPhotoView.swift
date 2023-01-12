858bc7a5303f52f8b7a4af0d972f410c21cd5dec
Uhucream <aborog1324@yahoo.co.jp>
2023-01-12 14:41:08 +0900

    feat: iOS/Scenes/CreateIDPhoto に、証明写真のサイズや色などを設定・作成する CreateIDPhotoView 追加

---
+++ iOS/Scenes/CreateIDPhoto/CreateIDPhotoView.swift
@@ -0,0 +1,140 @@
+//
+//  CreateIDPhotoView.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/01/09
+//  
+//
+
+import SwiftUI
+
+struct IDPhotoBackgroundColor {
+    let name: String
+    let color: Color
+}
+
+struct IDPhotoBackgroundColorPicker: View {
+    var availableBackgroundColors: [Color]
+    
+    @Binding var selectedBackgroundColor: Color
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 12) {
+            ForEach(availableBackgroundColors, id: \.self) { color in
+                ZStack {
+                    RoundedRectangle(cornerRadius: 4)
+                        .fill(color)
+                        .aspectRatio(1.0, contentMode: .fit)
+                        .frame(maxHeight: .infinity)
+                        .padding(4)
+                        .overlay {
+                            if color == selectedBackgroundColor {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.tintColor, lineWidth: 2)
+                                
+                                Image(systemName: "checkmark")
+                                    .resizable()
+                                    .padding()
+                                    .foregroundColor(.systemBackground)
+                            }
+                        }
+                }
+                .onTapGesture {
+                    selectedBackgroundColor = color
+                }
+            }
+        }
+    }
+}
+
+struct CreateIDPhotoView: View {
+    private let BACKGROUND_COLORS: [Color] = [Color(0x5FB8DE, alpha: 1.0), Color(0xA5A5AD, alpha: 1.0)]
+    
+    @State private var shouldShowSelectSizeSheet: Bool = false
+    
+    @Binding var selectedBackgroundColor: Color
+    
+    @Binding var previewUIImage: UIImage?
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Group {
+                if let previewUIImage = previewUIImage {
+                    Image(uiImage: previewUIImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Rectangle()
+                        .overlay {
+                            ProgressView()
+                        }
+                }
+            }
+            .frame(maxHeight: 280)
+            
+            Form {
+                Section {
+                    ScrollView(.horizontal) {
+                        IDPhotoBackgroundColorPicker(
+                            availableBackgroundColors: BACKGROUND_COLORS,
+                            selectedBackgroundColor: $selectedBackgroundColor
+                        )
+                        .frame(minHeight: 48)
+                        .padding()
+                    }
+                    .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                } header: {
+                    Text("背景色")
+                }
+                
+                Section {
+                    HStack {
+                        Text("サイズを選択")
+                            .foregroundColor(.tintColor)
+                        
+                        Spacer()
+                        
+                        Image(systemName: "chevron.up.chevron.down")
+                    }
+                    .onTapGesture {
+                        shouldShowSelectSizeSheet = true
+                    }
+                } header: {
+                    Text("サイズ")
+                }
+            }
+        }
+        .background {
+            Color
+                .systemGroupedBackground
+                .edgesIgnoringSafeArea(.all)
+        }
+        .sheet(isPresented: $shouldShowSelectSizeSheet) {
+            NavigationView {
+                IDPhotoSizePickerView()
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button(action: {
+                                
+                            }) {
+                                Text("閉じる")
+                            }
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct CreateIDPhotoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateIDPhotoView(
+            selectedBackgroundColor: .constant(
+                Color(0x5FB8DE, alpha: 1.0)
+            ),
+            previewUIImage: .constant(
+                .init(named: "TimCook")
+            )
+        )
+    }
+}
