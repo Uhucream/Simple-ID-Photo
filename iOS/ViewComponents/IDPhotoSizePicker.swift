2834c045903b7973417c9e860ff28a097b76b339
Uhucream <aborog1324@yahoo.co.jp>
2023-01-24 23:32:59 +0900

    fix: CreateIDPhotoView.swift 内に宣言していた IDPhotoSizePicker を、iOS/ViewComponents/ 下 に IDPhotoSizePicker.swift として移動

---
+++ iOS/ViewComponents/IDPhotoSizePicker.swift
@@ -0,0 +1,85 @@
+//
+//  IDPhotoSizePicker.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/01/24
+//  
+//
+
+import SwiftUI
+
+struct IDPhotoSizePicker: View {
+    @Binding var selectedIDPhotoSize: IDPhotoSizeVariant
+    
+    private func renderSelectionLabelText(_ sizeVariant: IDPhotoSizeVariant) -> Text {
+        if sizeVariant == .original {
+            return Text("オリジナル")
+        }
+        
+        if sizeVariant == .passport {
+            return Text("パスポート (35 x 45 mm)")
+        }
+        
+        let photoWidth: Int = Int(sizeVariant.photoSize.width.value)
+        let photoHeight: Int = Int(sizeVariant.photoSize.height.value)
+        
+        let unitSymbol: String = sizeVariant.photoSize.height.unit.symbol
+        
+        return Text("\(photoWidth) x \(photoHeight) \(unitSymbol)")
+    }
+    
+    var body: some View {
+        HStack {
+            ScrollViewReader { scrollViewProxy in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 4) {
+                        ForEach(IDPhotoSizeVariant.allCases, id: \.self) { sizeSelection in
+                            let isSelected: Bool = self.selectedIDPhotoSize == sizeSelection
+                            
+                            ZStack {
+                                renderSelectionLabelText(sizeSelection)
+                            }
+                            .font(.system(size: 14.0, design: .rounded))
+                            .fontWeight(.regular)
+                            .foregroundColor(
+                                isSelected ? .fixedWhite : .fixedLightGray
+                            )
+                            .padding(.horizontal, 6)
+                            .padding(.vertical, 1)
+                            .background {
+                                ZStack {
+                                    if isSelected {
+                                        BlurView(blurStyle: .systemChromeMaterialDark)
+                                            .background {
+                                                Color.fixedLightGray
+                                            }
+                                            .clipShape(Capsule())
+                                    }
+                                }
+                            }
+                            .onTapGesture {
+                                selectedIDPhotoSize = sizeSelection
+                            }
+                            .id(sizeSelection)
+                        }
+                    }
+                    .padding()
+                    .onAppear {
+                        if selectedIDPhotoSize == .original {
+                            return
+                        }
+                        
+                        scrollViewProxy.scrollTo(selectedIDPhotoSize)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct IDPhotoSizePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        IDPhotoSizePicker(selectedIDPhotoSize: .constant(.original))
+            .previewLayout(.sizeThatFits)
+    }
+}
