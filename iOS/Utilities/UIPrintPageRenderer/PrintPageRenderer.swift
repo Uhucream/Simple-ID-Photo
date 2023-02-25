d2f1a76dce12f6250977a11b525d554fa384c2da
Uhucream <aborog1324@yahoo.co.jp>
2023-02-25 19:36:43 +0900

    feat: 使用側で印刷内容の処理を自由に設定できるよう汎用性を高めた UIPrintPageRenderer のサブクラス、PrintPageRenderer を作成

---
+++ iOS/Utilities/UIPrintPageRenderer/PrintPageRenderer.swift
@@ -0,0 +1,54 @@
+//
+//  PrintPageRenderer.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/02/24
+//  
+//
+
+import UIKit
+
+class PrintPageRenderer: UIPrintPageRenderer {
+    static let APPLE_PRINT_PIXEL_DENSITY: Double = 72
+    
+    var onDrawPage: ((Int, CGRect) -> Void)?
+    var onDrawHeaderForPage: ((Int, CGRect) -> Void)?
+    var onDrawContentForPage: ((Int, CGRect) -> Void)?
+    var onDrawPrintFormatter: ((UIPrintFormatter, Int) -> Void)?
+    var onDrawFooterForPage: ((Int, CGRect) -> Void)?
+    
+    init(
+        onDrawPage: ((Int, CGRect) -> Void)? = nil,
+        onDrawHeaderForPage: ((Int, CGRect) -> Void)? = nil,
+        onDrawContentForPage: ((Int, CGRect) -> Void)? = nil,
+        onDrawPrintFormatter: ((UIPrintFormatter, Int) -> Void)? = nil,
+        onDrawFooterForPage: ((Int, CGRect) -> Void)? = nil
+    ) {
+        self.onDrawPage = onDrawPage
+        self.onDrawHeaderForPage = onDrawHeaderForPage
+        self.onDrawContentForPage = onDrawContentForPage
+        self.onDrawPrintFormatter = onDrawPrintFormatter
+        self.onDrawFooterForPage = onDrawFooterForPage
+    }
+    
+    
+    override func drawPage(at pageIndex: Int, in printableRect: CGRect) {
+        self.onDrawPage?(pageIndex, printableRect)
+    }
+
+    override func drawPrintFormatter(_ printFormatter: UIPrintFormatter, forPageAt pageIndex: Int) {
+        self.onDrawPrintFormatter?(printFormatter, pageIndex)
+    }
+
+    override func drawHeaderForPage(at pageIndex: Int, in headerRect: CGRect) {
+        self.onDrawHeaderForPage?(pageIndex, headerRect)
+    }
+
+    override func drawContentForPage(at pageIndex: Int, in contentRect: CGRect) {
+        self.onDrawContentForPage?(pageIndex, contentRect)
+    }
+
+    override func drawFooterForPage(at pageIndex: Int, in footerRect: CGRect) {
+        self.onDrawFooterForPage?(pageIndex, footerRect)
+    }
+}
