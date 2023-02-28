e6ffbb4eab186de39a4be5c45cf38cb7d7a01cf5
Uhucream <aborog1324@yahoo.co.jp>
2023-02-28 13:52:13 +0900

    style: Simple_ID_Photo.xcdatamodel ファイルの名前を SimpleIDPhoto に変更

--- iOS/Persistence.swift
+++ iOS/Persistence.swift
@@ -32,7 +32,7 @@ struct PersistenceController {
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "Simple_ID_Photo")
+        container = NSPersistentContainer(name: "SimpleIDPhoto")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
