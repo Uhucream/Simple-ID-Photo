f52a448428371aeba4063995812b8f8edd3dab92
Uhucream <aborog1324@yahoo.co.jp>
2023-03-02 09:51:16 +0900

    feat: 作成した Core Data のエンティティ GeneratedIDPhoto の生成を簡素化するため、convenience initializer を含んだ extension を作成

---
+++ iOS/Extensions/CoreData/ConvenienceInitializer/GeneratedIDPhoto+.swift
@@ -0,0 +1,34 @@
+//
+//  GeneratedIDPhoto+.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/03/01
+//  
+//
+
+import CoreData
+
+extension GeneratedIDPhoto {
+    convenience init(
+        on context: NSManagedObjectContext,
+        id: UUID = .init(),
+        createdAt: Date?,
+        imageURL: String?,
+        updatedAt: Date?,
+        appliedBackgroundColor: AppliedBackgroundColor? = nil,
+        appliedIDPhotoSize: AppliedIDPhotoSize? = nil,
+        sourcePhoto: SourcePhoto? = nil
+    ) {
+        self.init(context: context)
+        
+        self.id = id
+        
+        self.createdAt = createdAt
+        self.imageURL = imageURL
+        self.updatedAt = updatedAt
+        
+        self.appliedBackgroundColor = appliedBackgroundColor
+        self.appliedIDPhotoSize = appliedIDPhotoSize
+        self.sourcePhoto = sourcePhoto
+    }
+}
