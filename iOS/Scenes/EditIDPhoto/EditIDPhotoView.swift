c4f49b97f442b3796ba9c2df75a35c6446a901d2
Uhucream <aborog1324@yahoo.co.jp>
2023-03-09 22:22:57 +0900

    feat: 作成した証明写真を編集する画面、EditIDPhotoView を作成
    - 備考: CreateIDPhotoView を使い回して、Container だけ新規作成しようかと考えたが、柔軟性に欠けそうなので新規作成

---
+++ iOS/Scenes/EditIDPhoto/EditIDPhotoView.swift
@@ -0,0 +1,219 @@
+//
+//  EditIDPhotoView.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/03/07
+//  
+//
+
+import SwiftUI
+
+struct EditIDPhotoView: View {
+
+    @Binding var selectedProcess: IDPhotoProcessSelection
+    
+    @Binding var selectedBackgroundColor: Color
+    
+    @Binding var selectedIDPhotoSize: IDPhotoSizeVariant
+    
+    @Binding var previewUIImage: UIImage?
+    
+    @Binding var shouldDisableDoneButton: Bool
+    
+    var availableBackgroundColors: [Color]
+    
+    private(set) var onTapDismissButtonCallback: (() -> Void)? = nil
+
+    private(set) var onTapDoneButtonCallback: (() -> Void)? = nil
+    
+    func onTapDismissButton(action: @escaping () -> Void) -> Self {
+        var view = self
+        
+        view.onTapDismissButtonCallback = action
+        
+        return view
+    }
+    
+    func onTapDoneButton(action: @escaping () -> Void) -> Self {
+        var view = self
+        
+        view.onTapDoneButtonCallback = action
+        
+        return view
+    }
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            HStack {
+                ZStack {
+                    if selectedProcess == .backgroundColor {
+                        Text("背景色")
+                            .fontWeight(.light)
+                    }
+                    
+                    if selectedProcess == .size {
+                        Text("サイズ")
+                            .fontWeight(.light)
+                    }
+                }
+                .font(.subheadline)
+                .foregroundColor(.white)
+                .transaction { transaction in
+                    transaction.animation = .none
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
+            ZStack {
+                if let previewUIImage = previewUIImage {
+                    Image(uiImage: previewUIImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Rectangle()
+                        .overlay {
+                            ProgressView()
+                        }
+                }
+            }
+            .frame(maxHeight: 280)
+            
+            Spacer()
+            
+            VStack(spacing: 0) {
+                ZStack {
+                    if self.selectedProcess == .backgroundColor {
+                        HStack {
+                            Spacer()
+                            
+                            IDPhotoBackgroundColorPicker(
+                                availableBackgroundColors: self.availableBackgroundColors,
+                                selectedBackgroundColor: $selectedBackgroundColor
+                            )
+                            
+                            Spacer()
+                        }
+                    }
+                    
+                    if self.selectedProcess == .size {
+                        IDPhotoSizePicker(selectedIDPhotoSize: $selectedIDPhotoSize)
+                    }
+                }
+                .transaction { transaction in
+                    transaction.animation = .none
+                }
+                
+                HStack(alignment: .center) {
+                    Button(
+                        role: .destructive,
+                        action: {
+                            self.onTapDismissButtonCallback?()
+                        }
+                    ) {
+                        Label("終了", systemImage: "xmark")
+                            .labelStyle(.iconOnly)
+                            .padding()
+                    }
+                    .controlSize(.mini)
+                    
+                    Spacer()
+                    
+                    HStack(alignment: .center, spacing: 12) {
+                        Button(
+                            action: {
+                                var transaction: Transaction = .init(animation: .easeInOut)
+                                
+                                transaction.disablesAnimations = true
+                                
+                                withTransaction(transaction) {
+                                    self.selectedProcess = .backgroundColor
+                                }
+                            }
+                        ) {
+                            VStack(spacing: 2) {
+                                Label("背景色", systemImage: "paintbrush")
+                                    .labelStyle(.iconOnly)
+                                    .padding(8)
+                                
+                                Circle()
+                                    .frame(width: 4, height: 4)
+                                    .foregroundColor(self.selectedProcess == .backgroundColor ? .yellow : .clear)
+                            }
+                        }
+                        .tint(.white)
+                        
+                        Button(
+                            action: {
+                                var transaction: Transaction = .init(animation: .easeInOut)
+                                
+                                transaction.disablesAnimations = true
+                                
+                                withTransaction(transaction) {
+                                    self.selectedProcess = .size
+                                }
+                            }
+                        ) {
+                            VStack(spacing: 2) {
+                                Label("サイズ", systemImage: "person.crop.rectangle")
+                                    .labelStyle(.iconOnly)
+                                    .padding(8)
+                                
+                                Circle()
+                                    .frame(width: 4, height: 4)
+                                    .foregroundColor(self.selectedProcess == .size ? .yellow : .clear)
+                            }
+                        }
+                        .tint(.white)
+                    }
+                    
+                    Spacer()
+                    
+                    Button(
+                        action: {
+                            onTapDoneButtonCallback?()
+                        }
+                    ) {
+                        Image(systemName: "checkmark")
+                            .padding()
+                    }
+                    .tint(.yellow)
+                    .controlSize(.mini)
+                    .disabled(shouldDisableDoneButton)
+                }
+                .frame(maxHeight: 28)
+                .padding(.vertical)
+                .padding(.horizontal, 4)
+            }
+        }
+        .background {
+            Color
+                .black
+                .overlay(.bar)
+                .environment(\.colorScheme, .dark)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct EditIDPhotoView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditIDPhotoView(
+            selectedProcess: .constant(.backgroundColor),
+            selectedBackgroundColor: .constant(
+                Color.idPhotoBackgroundColors.blue
+            ),
+            selectedIDPhotoSize: .constant(.original),
+            previewUIImage: .constant(
+                .init(named: "SampleIDPhoto")
+            ),
+            shouldDisableDoneButton: .constant(true),
+            availableBackgroundColors: [
+                .idPhotoBackgroundColors.blue,
+                .idPhotoBackgroundColors.gray
+            ]
+        )
+        .previewDisplayName("Edit ID Photo View")
+    }
+}
