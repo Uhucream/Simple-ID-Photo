a375805360d98a22e1219c7c26b6156847b446a8
Uhucream <aborog1324@yahoo.co.jp>
2023-03-12 16:35:10 +0900

    feat: 「このアプリについて」画面の作成

---
+++ iOS/Scenes/Settings/SettingsAbout/SettingsAboutView.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsAboutView.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/03/12
+//  
+//
+
+import SwiftUI
+
+struct SettingsAboutView: View {
+    
+    @ScaledMetric(wrappedValue: 1, relativeTo: .body) private var twitterIconScaleFactor
+    
+    var body: some View {
+        Form {
+            Section {
+                Link(destination: URL(string: "twitter.com/Ukokkei95Toyama")!) {
+                    HStack(alignment: .center) {
+                        Image("TwitterIcon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxHeight: 16 * twitterIconScaleFactor)
+                        
+                        Text("@Ukokkei95Toyama")
+                            .font(.body)
+                    }
+                }
+            } header: {
+                Text("Developed By")
+            }
+        }
+    }
+}
+
+struct SettingsAboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsAboutView()
+    }
+}
