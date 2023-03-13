00fdd85f604811463a6985e95b61abc3fa713be1
Uhucream <aborog1324@yahoo.co.jp>
2023-03-13 22:58:57 +0900

    feat: UIPrintIntereactionControllerDelegate を汎用的に使えるようにしたクラス NormalPrinterPrintInteractionControllerDelegate の作成 style: フォルダがぐちゃぐちゃになりそうだったので、Utilities フォルダ下にあった UIPrintPageRenderer フォルダを Printing に改名し、NormalPrinterPrintInteractionControllerDelegate と PrintPageRenderer を格納

---
+++ iOS/Utilities/Printing/NormalPrinterPrintInteractionControllerDelegate.swift
@@ -0,0 +1,69 @@
+//
+//  NormalPrinterPrintInteractionControllerDelegate.swift
+//  Simple ID Photo
+//  
+//  Created by TakashiUshikoshi on 2023/03/13
+//  
+//
+
+import UIKit
+
+final class NormalPrinterPrintInteractionControllerDelegate: NSObject, UIPrintInteractionControllerDelegate {
+    
+    var choosePaper: ((UIPrintInteractionController, [UIPrintPaper]) -> UIPrintPaper)
+    
+    var onWillPresentPrinterOptions: ((UIPrintInteractionController) -> Void)?
+    var onDidPresentPrinterOptions: ((UIPrintInteractionController) -> Void)?
+    
+    var onWillDismissPrinterOptions: ((UIPrintInteractionController) -> Void)?
+    var onDidDismissPrinterOptions: ((UIPrintInteractionController) -> Void)?
+    
+    var onWillStartJob: ((UIPrintInteractionController) -> Void)?
+    var onDidFinishJob: ((UIPrintInteractionController) -> Void)?
+    
+    init(
+        choosePaper: @escaping ((UIPrintInteractionController, [UIPrintPaper]) -> UIPrintPaper),
+        onWillPresentPrinterOptions: ((UIPrintInteractionController) -> Void)? = nil,
+        onDidPresentPrinterOptions: ((UIPrintInteractionController) -> Void)? = nil,
+        onWillDismissPrinterOptions: ((UIPrintInteractionController) -> Void)? = nil,
+        onDidDismissPrinterOptions: ((UIPrintInteractionController) -> Void)? = nil,
+        onWillStartJob: ((UIPrintInteractionController) -> Void)? = nil,
+        onDidFinishJob: ((UIPrintInteractionController) -> Void)? = nil
+    ) {
+        self.choosePaper = choosePaper
+        self.onWillPresentPrinterOptions = onWillPresentPrinterOptions
+        self.onDidPresentPrinterOptions = onDidPresentPrinterOptions
+        self.onWillDismissPrinterOptions = onWillDismissPrinterOptions
+        self.onDidDismissPrinterOptions = onDidDismissPrinterOptions
+        self.onWillStartJob = onWillStartJob
+        self.onDidFinishJob = onDidFinishJob
+    }
+    
+    func printInteractionController(_ printInteractionController: UIPrintInteractionController, choosePaper paperList: [UIPrintPaper]) -> UIPrintPaper {
+        return choosePaper(printInteractionController, paperList)
+    }
+
+    func printInteractionControllerWillPresentPrinterOptions(_ printInteractionController: UIPrintInteractionController) {
+        self.onWillPresentPrinterOptions?(printInteractionController)
+    }
+
+    func printInteractionControllerDidPresentPrinterOptions(_ printInteractionController: UIPrintInteractionController) {
+        self.onDidPresentPrinterOptions?(printInteractionController)
+    }
+
+    func printInteractionControllerWillDismissPrinterOptions(_ printInteractionController: UIPrintInteractionController) {
+        self.onWillDismissPrinterOptions?(printInteractionController)
+    }
+
+    func printInteractionControllerDidDismissPrinterOptions(_ printInteractionController: UIPrintInteractionController) {
+        self.onDidDismissPrinterOptions?(printInteractionController)
+    }
+
+    func printInteractionControllerWillStartJob(_ printInteractionController: UIPrintInteractionController) {
+        self.onWillStartJob?(printInteractionController)
+    }
+
+    func printInteractionControllerDidFinishJob(_ printInteractionController: UIPrintInteractionController) {
+        self.onDidFinishJob?(printInteractionController)
+    }
+}
