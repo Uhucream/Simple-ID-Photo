38ce3bea25989a92d7504f426be2bb428c69a1ac
Uhucream <aborog1324@yahoo.co.jp>
2023-03-21 11:34:28 +0900

    feat: アプリ内課金関連のロジックを担うクラス、InAppPurchaseHelper を作成

---
+++ iOS/Utilities/StoreKit/InAppPurchaseHelper.swift
@@ -0,0 +1,120 @@
+//
+//  InAppPurchaseHelper.swift
+//  Simple ID Photo (iOS)
+//  
+//  Created by TakashiUshikoshi on 2023/03/19
+//  
+//
+
+import Combine
+import StoreKit
+
+public enum StoreError: Error {
+    case failedVerification
+}
+
+@MainActor
+final class InAppPurchaseHelper: ObservableObject {
+    
+    static let shared: InAppPurchaseHelper = .init()
+    
+    @Published private(set) var availableProducts: [Product] = []
+    
+    @Published private(set) var purchasedProductIdentifiers: Set<String> = []
+    
+    var transactionsListenerTask: Task<Void, Never>? = nil
+    
+    init() {
+        transactionsListenerTask = listenForTransactions()
+        
+        Task {
+            try await loadAvailableProducts()
+            
+            await refreshPurchasedProductIdentifiers()
+        }
+    }
+    
+    deinit {
+        transactionsListenerTask?.cancel()
+    }
+    
+    func loadAvailableProducts() async throws -> Void {
+        do {
+            let fetchedProducts: [Product] = try await Product.products(
+                for: [
+                    "pictures.uhucream_takashi.SimpleIDPhoto.test.hide_ads",
+                    "pictures.uhucream_takashi.SimpleIDPhoto.test.treat_item.beer"
+                ]
+            )
+            
+            await refreshPurchasedProductIdentifiers()
+            
+            self.availableProducts = fetchedProducts
+        } catch {
+            throw error
+        }
+    }
+    
+    func listenForTransactions() -> Task<Void, Never> {
+        return Task.detached(priority: .background) { [unowned self] in
+            for await updatesVerificationResult in Transaction.updates {
+                do {
+                    let transaction = try await checkVerified(result: updatesVerificationResult)
+                    
+                    await self.refreshPurchasedProductIdentifiers()
+                    
+                    await transaction.finish()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    func refreshPurchasedProductIdentifiers() async -> Void {
+
+        let currentEntitlements = Transaction.currentEntitlements
+        
+        for await currentEntitlement in currentEntitlements {
+
+            guard case .verified(let verifiedTransaction) = currentEntitlement else { return }
+            
+            if verifiedTransaction.productType == .consumable { return }
+            
+            let purchasedProduct: Product? = availableProducts.first { product in
+                return product.id == verifiedTransaction.productID
+            }
+            
+            guard let purchasedProduct = purchasedProduct else { return }
+            
+            self.purchasedProductIdentifiers.insert(purchasedProduct.id)
+        }
+    }
+    
+    func purchase(_ product: Product) async throws {
+        
+        let result = try await product.purchase()
+        
+        guard case .success(let verificationResult) = result else { return }
+        
+        guard case .verified(let verifiedTransaction) = verificationResult else { return }
+        
+        await verifiedTransaction.finish()
+        
+        await self.refreshPurchasedProductIdentifiers()
+    }
+    
+    //  MARK: https://developer.apple.com/documentation/storekit/in-app_purchase/implementing_a_store_in_your_app_using_the_storekit_api
+    func checkVerified<T>(result: VerificationResult<T>) throws -> T {
+        //Check whether the JWS passes StoreKit verification.
+        switch result {
+        case .unverified:
+            //StoreKit parses the JWS, but it fails verification.
+            throw StoreError.failedVerification
+
+        case .verified(let verifiedResult):
+            //The result is verified. Return the unwrapped value.
+            return verifiedResult
+        }
+    }
+}
