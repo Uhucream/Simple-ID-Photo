4fcab15ff5828b7c16d88896c66470c359137b65
Uhucream <aborog1324@yahoo.co.jp>
2023-03-27 11:58:54 +0900

    chore: Settings.bundle 内にライセンス表示を自動生成させるスクリプトと、それを実行する Build Phase の追加

---
+++ scripts/refresh_settings_bundle_acknowledgements.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+#  refresh_settings_bundle_acknowledgements.sh
+#  Simple ID Photo
+#
+#  Created by TakashiUshikoshi on 2023/03/27.
+#  
+
+
+if [[ $CI = "TRUE" ]]; then
+    cd $CI_WORKSPACE
+
+    /usr/local/bin/license-plist --output-path $CI_PRODUCT_PLATFORM/Settings.bundle --prefix Acknowledgements
+
+    exit 0
+fi
+
+cd "$SRCROOT"
+
+if [[ $TARGET_NAME = "Simple ID Photo (iOS)" ]]; then
+    /usr/local/bin/license-plist --output-path iOS/Settings.bundle --prefix Acknowledgements
+fi
