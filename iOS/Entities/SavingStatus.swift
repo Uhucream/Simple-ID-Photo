4af0b505c68b99a689e344d4116c494fddd03c07
Uhucream <aborog1324@yahoo.co.jp>
2023-04-20 09:27:04 +0900

    refactor: IDPhotoDetailViewContainer.swift 内にあった SavingStatus と SavingProgressView を別ファイルに分離

---
+++ iOS/Entities/SavingStatus.swift
@@ -0,0 +1,15 @@
+//
+//  SavingStatus.swift
+//  Simple ID Photo (iOS)
+//  
+//  Created by TakashiUshikoshi on 2023/04/20
+//  
+//
+
+import Foundation
+
+public enum SavingStatus {
+    case inProgress
+    case succeeded
+    case failed
+}
--- iOS/Scenes/IDPhotoDetail/IDPhotoDetailViewContainer.swift
+++ iOS/Scenes/IDPhotoDetail/IDPhotoDetailViewContainer.swift
@@ -10,56 +10,6 @@ import SwiftUI
 import Photos
 import Percentage
 
-enum SavingStatus {
-    case inProgress
-    case succeeded
-    case failed
-}
-
-fileprivate struct SavingProgressView: View {
-    
-    @Binding var savingStatus: SavingStatus
-    
-    var body: some View {
-        Group {
-            GeometryReader { geometry in
-                if savingStatus == .inProgress {
-                    ProgressView("保存しています")
-                        .foregroundColor(.label)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                }
-                
-                if savingStatus == .succeeded {
-                    VStack(spacing: 16) {
-                        Image(systemName: "checkmark")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxHeight: 30%.of(geometry.size.height))
-                        
-                        Text("保存しました!")
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                }
-                
-                if savingStatus == .failed {
-                    VStack(spacing: 16) {
-                        Image(systemName: "exclamationmark")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxHeight: 30%.of(geometry.size.height))
-                        
-                        Text("失敗しました")
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                }
-            }
-            .font(.title3)
-        }
-        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16))
-        .aspectRatio(1, contentMode: .fit)
-    }
-}
-
 struct IDPhotoDetailViewContainer: View {
     
     @Environment(\.managedObjectContext) private var viewContext
