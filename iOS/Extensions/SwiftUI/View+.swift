bb2055bc967b4cbd7c143743b680a927f410396f
Uhucream <aborog1324@yahoo.co.jp>
2023-04-21 22:38:58 +0900

    feat: View+ に、リバースマスクを実現する関数 reverseMask() を追加

--- iOS/Extensions/SwiftUI/View+.swift
+++ iOS/Extensions/SwiftUI/View+.swift
@@ -51,4 +51,18 @@ extension View {
             }
         )
     }
+    
+    func reverseMask<Mask: View>(
+        alignment: Alignment = .center,
+        @ViewBuilder _ mask: () -> Mask
+    ) -> some View {
+        self.mask {
+            Rectangle()
+                .overlay(alignment: alignment) {
+                    mask()
+                        .blendMode(.destinationOut)
+                }
+                .compositingGroup()
+        }
+    }
 }
