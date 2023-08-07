9e9f8c44f7146d32694d3e8c299b25402186c08a
Uhucream <aborog1324@yahoo.co.jp>
2023-08-07 16:20:36 +0900

    feat: Calendar に .gregorian な Calendar のスタティックプロパティを追加する extension 作成

---
+++ iOS/Extensions/Foundation/Calendar+.swift
@@ -0,0 +1,14 @@
+//
+//  Calendar+.swift
+//  Simple ID Photo
+//
+//  Created by Work on 2023/08/07.
+//
+
+import Foundation
+
+extension Calendar {
+    static var gregorian: Self {
+        .init(identifier: .gregorian)
+    }
+}
