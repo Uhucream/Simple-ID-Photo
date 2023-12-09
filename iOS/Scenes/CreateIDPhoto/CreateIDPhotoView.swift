1358758e2ab42c4cb7dfb2ddcd4d6e1ce812b5b9
Uhucream <aborog1324@yahoo.co.jp>
2023-12-10 03:12:50 +0900

    fix: CreateIDPhotoView において、サイズ変更の際に画像位置のアニメーションがかからない問題が発生していたので修正

--- iOS/Scenes/CreateIDPhoto/CreateIDPhotoView.swift
+++ iOS/Scenes/CreateIDPhoto/CreateIDPhotoView.swift
@@ -354,6 +354,10 @@ struct CreateIDPhotoView: View {
                                 }
                                 .padding(.horizontal, CROP_VIEW_IMAGE_HORIZONTAL_PADDING)
                                 .offset(previewImageOffset)
+                                .animation(
+                                    .easeOutQuart(duration: CROP_VIEW_ANIMATION_DURATION_SECONDS),
+                                    value: previewImageOffset
+                                )
                                 .scaleEffect(previewImageViewScalingAmount)
                                 .position(
                                     x: croppingFrameProxy[namedAnchor.anchor].midX,
